2b82b0cc4930c4b9009f14d383b9344f137f4645
Reza <alialreza98@gmail.com>
2022-07-29 01:49:20 +0600

    pizza card overflow

---
+++ components/PizzaList.jsx
@@ -0,0 +1,31 @@
+import styles from "../styles/PizzaList.module.css";
+
+import React from "react";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quis
+        dolorem tempore veniam error, asperiores accusantium itaque quas fugit
+        fuga excepturi doloremque eaque blanditiis alias aliquam natus voluptate
+        optio cumque.
+      </p>
+
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
