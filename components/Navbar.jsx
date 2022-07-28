5849c4275a971657cf189129d3d1a2e31eb2f37d
Reza <alialreza98@gmail.com>
2022-07-28 19:28:55 +0600

    navbar flex display

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,17 +4,17 @@ import styles from "../styles/Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className="styles item">
-        <div className="styles callButton">
+      <div className={styles.item}>
+        <div className={styles.callButton}>
           <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
-        <div className="styles texts">
-          <div className="styles text">Order Now!!!</div>
-          <div className="styles text">012 345 678</div>
+        <div className={styles.texts}>
+          <div className={styles.text}>Order Now!!!</div>
+          <div className={styles.text}>012 345 678</div>
         </div>
       </div>
-      <div className="styles item"></div>
-      <div className="styles item"></div>
+      <div className={styles.item}>Center</div>
+      <div className={styles.item}>right</div>
     </div>
   );
 };
