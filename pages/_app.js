9fdb48ea68673b78735d3058bd0bf363eaaa142e
Reza <alialreza98@gmail.com>
2022-07-28 15:48:51 +0600

    all page same component

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
+    // layout component will show all pages thats way we it like that
     <Layout>
       <Component {...pageProps} />
     </Layout>
