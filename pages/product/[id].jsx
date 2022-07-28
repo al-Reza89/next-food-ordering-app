9d815b02338017246ae1d91266672e8ea5d19eec
Reza <alialreza98@gmail.com>
2022-07-29 05:29:28 +0600

    size page

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Product.module.css";
 
 const Product = () => {
+  const [size, setSize] = useState(0);
+
   const pizza = {
     id: 1,
     img: "/img/pizza.png",
@@ -15,10 +17,29 @@ const Product = () => {
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.imgContainer}>
-          <Image src={pizza.img} layout="fill" alt="" />
+          <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
         </div>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className="styles.price">${pizza.price[size]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose Your Size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size}>
+            <Image src={"/img/size.png"} layout="fill" alt="" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size}>
+            <Image src={"/img/size.png"} layout="fill" alt="" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size}>
+            <Image src={"/img/size.png"} layout="fill" alt="" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
