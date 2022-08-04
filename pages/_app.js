b90b0a9e8e413f6119b3799ecbaf02a090c1c0f3
Reza <alialreza98@gmail.com>
2022-08-05 05:09:10 +0600

    redix toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
+import { Provider } from "react-redux";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import store from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   return (
     // layout component will show all pages thats way we it like that
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
