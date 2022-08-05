f4a88a28ab80fdef2d1c50fd05eebeafe35ed47a
Reza <alialreza98@gmail.com>
2022-08-05 16:22:05 +0600

    basic structure for paypal

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -2,12 +2,72 @@ import Image from "next/image";
 import React from "react";
 import styles from "../styles/Cart.module.css";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
 
-  console.log(cart);
+  const amount = "2";
+  const currency = "USD";
+  const style = { layout: "vertical" };
+
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, [currency, showSpinner]);
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function () {
+              // Your code here after capture the order
+            });
+          }}
+        />
+      </>
+    );
+  };
+
+  // console.log(cart);
 
   return (
     <div className={styles.container}>
@@ -73,6 +133,15 @@ const Cart = () => {
             <b className={styles.totalTextTitle}>Total:</b>${cart.total}
           </div>
           <button className={styles.button}>CHECKOUT NOW!</button>
+          <PayPalScriptProvider
+            options={{
+              "client-id": "test",
+              components: "buttons",
+              currency: "USD",
+            }}
+          >
+            <ButtonWrapper currency={currency} showSpinner={false} />
+          </PayPalScriptProvider>
         </div>
       </div>
     </div>
